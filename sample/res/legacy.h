5271af4ac8bf3151f92c52c2479c76e87ee89ed4
白い猫 <shiroineko.tfs@gmail.com>
2023-11-09 14:53:22 +0700

    remove samole

--- sample/res/legacy.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-#ifndef _CPP_KERNEL_HEADER_TEST_legacy
-#define _CPP_KERNEL_HEADER_TEST_legacy
-
-#endif
--- sample/res/linker.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-#ifndef _CPP_KERNEL_HEADER_TEST_linker
-#define _CPP_KERNEL_HEADER_TEST_linker
-
-#endif
