00895da90d83093729ff558b6963cabb616605d6
白い猫 <shiroineko.tfs@gmail.com>
2023-11-09 14:53:30 +0700

    update prop

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-      name='jupyter-cpp-extension-template',
+      name='jupyter-cpp-qiskit',
       version='1.0.0a5',
-      description='C++ kernel for Jupyter headers template. Used to provide additional abilities for the kernel.',
+      description='Qiskit header for C++ Jupyter kernel. Provide quantum computing services for Jupyter, written in C++.',
       author='Shiroi Neko',
       author_email='shiroineko.tfs@gmail.com',
       license='MIT',
@@ -16,11 +16,12 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Operating System :: Microsoft :: Windows'
       ],
-      url='https://github.com/shiroinekotfs/jupyter-cpp-header-template',
+      url='https://github.com/shiroinekotfs/jupyter-cpp-qiskit',
       include_package_data=True,
       install_requires = ['jupyter-cpp-kernel>=1.0.0a5'],
       data_files = [
-          ("share/cpp_header", ['sample/sample.hpp']),
-          ("share/cpp_header/res", ['sample/res/legacy.h', 'sample/res/linker.hpp'])
+          ("share/cpp_header", 
+           ['jupyter-cpp-qiskit/qiskit.hpp']
+          )
       ]
 )
